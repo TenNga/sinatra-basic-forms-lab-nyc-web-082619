1840106dc8fd77f0dd148d86fae3fd9a2b63a612
Kevin Karma <kevinkarma55@gmail.com>
2019-09-17 21:53:59 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,8 @@
-class Puppy << ActiveRecord 
+class Puppy < ActiveRecord::Base 
+  attr_accessible :name, :breed, :months_old
+  def initialize(name,breed,months_old)
+    @name = name 
+    @breed = breed
+    @months_old = months_old
+  end
+end
